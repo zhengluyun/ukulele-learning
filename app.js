67090c9c664ddcef23a465f8694fcc14e80867f9
zhengluyun <zlystevens@gmail.com>
2020-06-13 11:47:39 -0400

    create a new angular project

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-const http = require('http');
+// const http = require('http');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+// const hostname = '127.0.0.1';
+// const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.end('Hello World');
-});
+// const server = http.createServer((req, res) => {
+//   res.end('Hello World');
+// });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
